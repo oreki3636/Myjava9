5b7570397ed1a08af43f7b726ffad2fd84f776d7
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-12-04 06:17:46 +0000

    Lesson8_2完了

---
+++ Lesson8_2.java
@@ -0,0 +1,21 @@
+class Lesson8_2 {
+  public static void main(String[] args) {
+    String[] arr = {"sato","suzuki","takahashi"};
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+    // String[] arr;
+    // arr = new String[3];
+
+
+    // arr[0]="sato";
+    // arr[1]="suzuki";
+    // arr[2]="takahashi";
+
+    // System.out.println(arr[0]);
+    // System.out.println(arr[1]);
+    // System.out.println(arr[2]);
+
+  }
+}
