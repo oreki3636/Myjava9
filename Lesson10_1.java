19023db514dd626bf95d53b617e53fee5dd297c8
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-12-13 07:15:14 +0000

    Lesson11_1

--- Lesson10_1.java
+++ Lesson10_1.java
@@ -1,18 +1,48 @@
 class Lesson10_1 {
-  public static void main(String[] args) {
-    int age = 22;
+  public static void main(String[] age) {
 
-    if(age >=20){
+    int age3 = 0;
+
+    if (age3 >= 20) {
       System.out.println("adult");
-    }
-
-    int agee = 18;
-    if(agee >= 20){
+    }else if (age3==0){
+      System.out.println("baby");
+    }else {
       System.out.println("child");
     }
   }
 
-  // // if(条件){
-  //   条件を満たした時の処理
-  // }
-}
+
+
+      //   int age = 18;
+
+      //   if (age >= 20) {
+      //     System.out.println("adult");
+      //   }else {
+      //     System.out.println("child");
+      //   }
+      // }
+
+
+
+
+
+
+
+    //   int age = 22;
+
+    //   if(age >=20){
+    //     System.out.println("adult");
+    //   }
+
+
+
+
+
+    //   int agee = 18;
+    //   if(agee >= 20){
+    //     System.out.println("child");
+    //   }
+    // }
+
+  }
