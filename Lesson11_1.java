812382f162d42d9754190f6e5fd4fe67cede32fc
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-12-14 03:56:03 +0000

    Lesson11

--- Lesson11_1.java
+++ Lesson11_1.java
@@ -3,5 +3,68 @@
     // for(int i = 0;i <= 4;i++){
     // }
     // for(スタート値;継続条件;増減式){
-      // スタート値=カウンタ変数
+      // スタート値=カウンタ変数(iを使うことが多い)
+
     // }
+
+class Lesson11_1 {
+  public static void main(String[] args) {
+
+
+
+
+
+
+// 変数
+    // int arr[] = {2,4,6,8,10};
+    // int sum = 0;
+
+    // for( int i = 0; i <= 4; i++) {
+    //   sum += arr[i];
+    // }
+    //   System.out.println(sum);
+
+
+// 配列
+    // int arr[] = {2,4,6,8,10};
+
+    // for( int i = 0; i <= 4; i++) {
+    //   System.out.println(arr[i]);
+    // }
+
+
+// for文のネスト
+    //   for(int i = 0; i <= 2; i++) {
+    //     for(int j = 0; j <= 2; j++) {
+    //       System.out.println( i + "-" + j);
+    //     }
+    //   }
+
+
+// for文
+  //   for(int i = 0; i <= 4; i++){
+  //     System.out.println(i);
+  //   }
+  // }
+
+
+// break文
+  //   for(int i = 0; i <= 4; i++){
+  //     if(i==3){
+  //       break;
+  //       // 繰り返しを終了
+  //     }
+  //     System.out.println(i);
+  //   }
+
+
+// continue文
+  //   for(int i = 0; i <= 4; i ++) {
+  //     if(i==3){
+  //       continue;
+  //     }
+  //     System.out.println(i);
+  //   }
+
+  }
+}
